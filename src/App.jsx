63d42b0bd6cecab590203e477feb053ef4dd8ae6
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-08-12 18:04:26 -0500

    Route nesting test implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Link,
+  Outlet,
+} from "react-router-dom";
 import "./style.css";
 
 function App() {
@@ -9,6 +16,11 @@ function App() {
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="mainApp" element={<MainApp />}>
+          <Route index element={<MainAppCities />} />
+          <Route path="cities" element={<MainAppCities />} />
+          <Route path="countries" element={<MainAppList />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
@@ -54,7 +66,48 @@ function Login() {
   return (
     <>
       <Navbar />
-      <div style={{ backgroundColor: "red" }}>Login</div>
+      <div style={{ backgroundColor: "red" }}>
+        <p>Login</p>
+        <Link to="/mainApp">Login</Link>
+      </div>
+    </>
+  );
+}
+
+function MainAppNav() {
+  return (
+    <>
+      <NavLink to="/mainApp/cities">Cities</NavLink>
+      <NavLink to="/mainApp/countries">Countries</NavLink>
+    </>
+  );
+}
+
+function MainApp() {
+  return (
+    <>
+      <div>
+        <Link to="/homepage">Homepage</Link>
+        <p>App</p>
+        <MainAppNav />
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+function MainAppCities() {
+  return (
+    <>
+      <div>Cities</div>
+    </>
+  );
+}
+
+function MainAppList() {
+  return (
+    <>
+      <div>List</div>
     </>
   );
 }
