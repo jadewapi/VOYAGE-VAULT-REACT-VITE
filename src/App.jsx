2faf8f29924ffbc3204cf0783e58b07feb3d400f
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-08-14 16:47:37 -0500

    useParam

--- src/App.jsx
+++ src/App.jsx
@@ -1,172 +1,55 @@
-import { useEffect, useState } from "react";
+import "./style.css";
 import {
   BrowserRouter,
   Routes,
   Route,
-  NavLink,
   Link,
+  NavLink,
   Outlet,
 } from "react-router-dom";
-import "./style.css";
 
 function App() {
-  const [places, setPlaces] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const BASE_URL = "http://localhost:8000";
-
-  useEffect(function () {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        if (!res.ok) {
-          throw new Error("error with fetching data.");
-        }
-        setPlaces(data);
-      } catch (error) {
-        console.log(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchData();
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="homepage" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="mainApp" element={<MainApp />}>
-          <Route
-            index
-            element={<MainAppCities place={places} isLoading={isLoading} />}
-          />
-          <Route
-            path="cities"
-            element={<MainAppCities places={places} isLoading={isLoading} />}
-          />
-          <Route path="cities/:id" element={<ClickedCity />}></Route>
-          <Route
-            path="countries"
-            element={
-              <MainAppCountryList places={places} isLoading={isLoading} />
-            }
-          />
+        <Route index element={<MainApp />} />
+        <Route path="/" element={<MainApp />}>
+          <Route path="application" element={<Application />}>
+            <Route path=":cities" element={<Cities />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-function ClickedCity() {
-  return <p>A city is clicked</p>;
+function Cities() {
+  return <div>List of cities</div>;
 }
 
-function MainAppCities({ places, isLoading }) {
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (!isLoading && places) {
-    return places.map((item) => (
-      <Link key={item.id} to={item.id}>
-        <p>{item.cityName}</p>
-      </Link>
-    ));
-  }
-  <ClickedCity />;
-}
-
-function MainAppCountryList({ places, isLoading }) {
-  const countries = places.reduce((arr, curr) => {
-    if (!arr.map((obj) => obj.country).includes(curr.country)) {
-      return [...arr, curr];
-    } else return arr;
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-  if (!isLoading && places) {
-    return countries.map((item) => (
-      <Link key={item.id} to={item.id}>
-        <p>{item.country}</p>
-      </Link>
-    ));
-  }
-}
-
-function Navbar() {
-  return (
-    <nav style={{ backgroundColor: "green" }}>
-      <ul>
-        <li>
-          <NavLink to="/homepage">Homepage</NavLink>
-        </li>
-        <li>
-          <NavLink to="/pricing">Pricing</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-function Homepage() {
+function Application() {
   return (
     <>
-      <Navbar />
-      <div style={{ backgroundColor: "blue", color: "white" }}>Homepage</div>
+      <p>Application</p>
+      <button>
+        <NavLink to={"8739287398"}>City</NavLink>
+      </button>
+      <button>
+        <NavLink>Countries</NavLink>
+      </button>
+      <Outlet />
     </>
-  );
-}
-
-function Pricing() {
-  return (
-    <>
-      <Navbar />
-      <div style={{ backgroundColor: "orange" }}>Pricing</div>
-    </>
-  );
-}
-
-function Login() {
-  return (
-    <>
-      <Navbar />
-      <div style={{ backgroundColor: "red" }}>
-        <p>Login</p>
-        <Link to="/mainApp">Login</Link>
-      </div>
-    </>
-  );
-}
-
-function MainAppNav() {
-  return (
-    <div>
-      <NavLink to="/mainApp/cities">Cities</NavLink>
-      <NavLink to="/mainApp/countries">Countries</NavLink>
-    </div>
   );
 }
 
 function MainApp() {
   return (
     <>
-      <div>
-        <Link to="/homepage">Homepage</Link>
-        <p>-------------</p>
-        <p>App</p>
-        <MainAppNav />
-        <Outlet />
-      </div>
+      <div>Main app</div>
+      <button>
+        <Link to="application">Login</Link>
+      </button>
+      <Outlet />
     </>
   );
 }
